3346e8f5e9cf575a0ff25f44c3d365fe780cfb27
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-03-01 10:29:47 -0300

    Segundo.

---
+++ unidade_1/EnunciadoB.java
@@ -0,0 +1,14 @@
+public class EnunciadoB {
+  public static void main(String[] args) {
+    // Entrada 
+    int largura = 5;
+    int altura = 3;
+
+    // Processo
+    int area = largura * altura;
+
+    //  Saída
+    System.out.println("Área: "+area);
+
+  }
+}
