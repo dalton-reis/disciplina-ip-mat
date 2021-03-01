23b7bf4a2b7a7d008e7a1a0a6009b921405def02
Dalton Solano dos Reis <dalton.reis@gmail.com>
2021-03-01 08:56:02 -0300

    Fazendo.

---
+++ unidade_1/EnunciadoA.java
@@ -0,0 +1,15 @@
+public class EnunciadoA {
+  public static void main(String[] args) {
+    // Entradas
+    int nota1 = 7;
+    int nota2 = 8;
+    int nota3 = 9;
+
+    // Processos
+    int media = (nota1 + nota2 + nota3) / 3;
+
+    // Saída
+    System.out.println("Média: "+media);
+
+  }
+}
